25d72473d1854958623183967c2f0d6f7d17d389
Tim Scott <118086790+timdev-dash@users.noreply.github.com>
2024-08-13 18:28:09 -0400

    Part 1 Complete

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -49,4 +49,67 @@ def process_data(model_pred_df, genres_df):
         genre_fp_counts (dict): Dictionary of false positive genre counts
     '''
 
-    # Your code here
+    # Create list of genres
+    genre_list: list = []
+
+    # Begin loop to fill list
+    row_num: int = 0
+
+    while row_num < genres_df.shape[0]:
+
+        # Adding the genre to the list
+        genre_list.append(genres_df.iloc[row_num, 0])
+
+        # Iterating row_num
+        row_num += 1
+
+    # Building the lists used to create dictionaries to return
+    dict_keys: list = []
+    dict_values: list = []
+
+    # Popluating dictionaries with the list of genres
+    for genre in genre_list:
+        dict_keys.append(genre)
+        dict_values.append(0)
+
+    # Creating dictionaries of true genre counts, true positive genre counts, and 
+    # fales positive genre counts.
+    genre_true_counts: dict = dict(zip(dict_keys, dict_values))
+    genre_tp_counts: dict = dict(zip(dict_keys, dict_values))
+    genre_fp_counts: dict = dict(zip(dict_keys, dict_values))
+
+    # Begin loop to record true, tp, and fp counts
+    movie_index: int = 0
+
+    while movie_index < model_pred_df.shape[0]:
+
+        # Setting predicted and actual genres
+        predicted_genre: str = model_pred_df.iloc[movie_index, 1]
+        actual_genres: list = ((model_pred_df.iloc[movie_index, 2].strip("[]")).strip("'")).split("', '")
+
+        # Counting true and false positives
+        if model_pred_df.iloc[movie_index, 3] == 0:
+            genre_fp_counts[predicted_genre] += 1
+        elif model_pred_df.iloc[movie_index, 3] == 1:
+            genre_tp_counts[predicted_genre] += 1
+        
+        # Counting true genres
+        for movie_genre in actual_genres:
+            if movie_genre == '':
+                print('Blank genre found in movie ' + model_pred_df.iloc[movie_index, 0])
+                continue
+            genre_true_counts[movie_genre] += 1
+
+        movie_index += 1
+
+    return genre_list, genre_true_counts, genre_tp_counts, genre_fp_counts
+
+
+if __name__ == '__main__':
+
+    
+    # Load data from CSV files
+    model_pred_df, genres_df = load_data()
+    
+    # Process data to get genre counts and predictions
+    genre_list, genre_true_counts, genre_tp_counts, genre_fp_counts = process_data(model_pred_df, genres_df)
